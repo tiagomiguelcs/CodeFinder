9266adda597c90f5491788fdd51876f291214c32
Tiago Simões <tiago.simoes@it.ubi.pt>
2020-12-23 13:20:34 +0000

    Added app permissions request.

--- app/src/main/java/com/teaching/codefinder/MainActivity.java
+++ app/src/main/java/com/teaching/codefinder/MainActivity.java
@@ -1,11 +1,38 @@
-/* */
+/* ---------------------------------------------------------------------------
+//
+//	CodeFinder
+//
+//  Copyright (C) 2020 Instituto de Telecomunicações (www.it.pt)
+//  Copyright (C) 2020 Universidade da Beira Interior (www.ubi.pt)
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// 'images: Flaticon.com'. The logo of the application has been designed
+//  using resources from Flaticon.com.
+// ---------------------------------------------------------------------------
+*/
 package com.teaching.codefinder;
 import java.io.InputStream;
 import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -24,8 +51,21 @@ public class MainActivity extends AppCompatActivity {
     /** Praise the class constructor */
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); // Avoid dark mode
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Handle permissions */
+    @Override public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    selectCSV();
+                }else
+                    this.finishAffinity();
+                return;
+            }
+        }
     }
 
     /** Handle the selection of a file */
@@ -49,8 +89,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /** Ask permissions */
+    public void askPerm(View view){
+        // If necessary, request a dialog box to check if the user allows the app to read a file from storage.
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        }else {
+            selectCSV();
+        }
+    }
+
     /** Select the CSV file */
-    public void selectCSV(View view){
+    public void selectCSV(){
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setType("*/*");
--- app/src/main/java/com/teaching/codefinder/Reader.java
+++ app/src/main/java/com/teaching/codefinder/Reader.java
@@ -1,3 +1,27 @@
+/* ---------------------------------------------------------------------------
+//
+//	CodeFinder
+//
+//  Copyright (C) 2020 Instituto de Telecomunicações (www.it.pt)
+//  Copyright (C) 2020 Universidade da Beira Interior (www.ubi.pt)
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// 'images: Flaticon.com'. The logo of the application has been designed
+//  using resources from Flaticon.com.
+// ---------------------------------------------------------------------------
+*/
 package com.teaching.codefinder;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,7 +50,7 @@ public class Reader {
             }
         }
         catch (IOException ex) {
-            throw new RuntimeException("Error in reading CSV file: "+ex);
+            throw new RuntimeException("Error while reading CSV file: "+ex);
         }
         finally {
             try {
